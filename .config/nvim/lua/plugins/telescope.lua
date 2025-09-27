5e2f1d119b5f13db560ffc86b6a1d7b181f54100
Axel <axelpascual83@gmail.com>
2025-09-27 14:32:34 +0200

    nvim config

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup({})
+    end,
+  },
+}
